e9461d19dfb19b84b1434653fa89b79f3bf28ee3
Sarah Obarhoro <obarhorosarah@gmail.com>
2025-06-26 18:48:07 +0100

    Updated server.js

--- src/server.js
+++ src/server.js
@@ -44,5 +44,4 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-  console.log(`Health check: http://localhost:${PORT}/health`);
 })
